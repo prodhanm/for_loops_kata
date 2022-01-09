41d67af330df480621c4aa7a08306193be830e38
Mustafizur <mustafizur.prodhan@gmail.com>
2022-01-08 23:21:59 -0500

    counter math in kata.

--- for_loop.py
+++ for_loop.py
@@ -1,7 +1,7 @@
 print("for_loops kata")
 print("1. for loops usage of arithmetics.")
 print("a. for_loops addition")
-num_range = range(16)
+num_range = range(1, 16)
 for num in num_range:
     print(num + 3)
 
@@ -24,3 +24,28 @@ for num in num_range:
 print("f. modulo.")
 for num in num_range:
     print(f"The remainder of {num} is {num%3}")
+
+print("2. Counting by artihmatic.")
+print("a. addition ")
+counter = 0
+for num in num_range:
+    counter += num
+print(f"The total sum is {counter}.")
+
+
+print("b. multiplication ")
+product = 1
+for num in num_range:
+    product *= num
+print(f"The product is {product}.")
+
+print("c. division ")
+product = 1
+for num in num_range:
+    product /= num
+print(f"The quotient is {product}.")
+
+
+
+
+
